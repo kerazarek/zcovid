597ed48c67e0f58ddb8a18425218166ecbdd7bc0
Zarek Siegel <zarek.siegel@gmail.com>
2020-03-22 12:42:06 -0700

    init

---
+++ list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+def main():
+¿
+    import sys
+
+    print(sys.stdin.read())
+
+
+if __name__ == "__main__":
+    main()
---
+++ list.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#####
+    #
+    # (c) Zarek Siegel
+    # created 03/14/20 22:51
+
+echo $(tree -f covid19)
+    # | python \
+        # -c \
+            # 'print("hi")'
+
+
